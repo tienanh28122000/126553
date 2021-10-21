ea641a8a9d7107c5f36ffc0d31b8994c83629d61
tienanh28122000 <58310317+tienanh28122000@users.noreply.github.com>
2021-10-21 21:36:19 +0700

    first commit

---
+++ HelloToast_partA/app/src/main/java/vn/edu/hust/sis/anhnmt187213/hellotoast/MainActivity.java
@@ -0,0 +1,31 @@
+package vn.edu.hust.sis.anhnmt187213.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.showCount);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        mShowCount.setText(Integer.toString(mCount));
+    }
+}
